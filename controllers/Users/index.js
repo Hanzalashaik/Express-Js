0ece8809436748ff540c0e70ad0c4ce60acf20f5
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-10-11 05:42:10 +0000

    In Router (Get all user,Delete one user,Delete all user)

--- controllers/Users/index.js
+++ controllers/Users/index.js
@@ -32,7 +32,7 @@ router.put("/update/:email", async (req, res) => {
   try {
     let UserEmail = req.params.email;
     let updateEmail = req.body;
-    
+
     let getuserData = await userModel.findOneAndUpdate(
       { email: UserEmail },
       { $set: updateEmail },
@@ -44,6 +44,50 @@ router.put("/update/:email", async (req, res) => {
     res.status(200).json(getuserData);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//get specific user by email
+
+router.get("/getuserbyemail/:email", async (req, res) => {
+  try {
+    let email = req.params.email;
+
+    let getUser = await userModel.findOne({ email: email }); //or we only write {email} if key and value is same
+
+    if (!getUser) {
+      res.status(401).json({ msg: "User not found" });
+    }
+    res.status(200).json(getUser);
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error" });
+  }
+});
+
+//Delete specific User
+
+router.delete("/deleteUser/:email", async (req, res) => {
+  try {
+    let email = req.params.email;
+
+    let findEmail = await userModel.deleteOne({ email });
+    if (!findEmail) {
+      res.status(401).json({ msg: "Email not found" });
+    }
+    res.status(200).json({ msg: "User Deleted Sucessfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Internel server Error" });
+  }
+});
+
+// Delete All Users
+
+router.delete("/deleteAll", async (req, res) => {
+  try {
+    await userModel.deleteMany({});
+    res.status(200).json({ msg: "All Users Deleted Sucessfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error" });
   }
 });
 
