5e564e4464392b55898a56f321e00bc9d6b9da12
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-10-11 05:09:43 +0000

    middleware and error handler added

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ import "./utils/dbConnect.js"
 const app=express();
 const PORT = config.get("PORT")
 
+//Build In Middleware
 app.use(express.json());
 
+//for our purpose
 app.get("/",(req,res)=>{
     res.status(200).send("<h1>Hello Server is Running 🚀</h1>");
 })
 
-//calling route post 
-
+//calling routes
 app.use("/users",userRouter)
+
+//Error Handler
+app.use((req,res)=>{
+    res.status(404).send("Not Found -Invalid Route")
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Sever is running on port ${PORT}`);
